0b1324c785dc5d381de1084e0d82df4259141cee
Slcode92 <84865384+Slcode92@users.noreply.github.com>
2022-12-29 22:20:31 +0000

    Working with File and FileInfo classes

---
+++ FileAndFileInfo/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+
+namespace FileAndFileInfo
+{
+    class Program
+
+    //File- Provides static methods 
+
+    //File Info - Provides instance methods
+
+
+    {
+        static void Main(string[] args)
+        {
+            var path = @"c:\somefile.jpg";
+
+            File.Copy(@"c:\temp\myfile.jpg", @"d:\temp\myfile.jpg", true);
+            //(source, destination, true-if file exists we can overwrite it)
+            
+            File.Delete(path);
+
+            if (File.Exists(path))
+            { 
+                //do something 
+            
+            }
+
+
+            //Read all text in files-method returns a string 
+            var content= File.ReadAllText(path);
+
+
+
+            //created object
+            var fileInfo = new FileInfo(path);
+            fileInfo.CopyTo("...");
+            fileInfo.Delete();
+
+            //check if file exists 
+            if (fileInfo.Exists)
+            {
+                //do something 
+            }
+
+
+            
+
+
+        }
+    }
+}
+ 
