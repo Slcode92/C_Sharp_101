869cf426a7d679e2b916415cbe035a7bb1889d21
Slcode92 <84865384+Slcode92@users.noreply.github.com>
2022-12-29 22:57:54 +0000

    Working with fields

---
+++ Fields/Customer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace Fields
+{
+    public class Customer 
+    {
+        public int Id;
+        public string Name;
+        public readonly List<Order> Orders = new List<Order>(); //no matter what constructor is used the orders field will be called on a empty list 
+        
+        public Customer(int id)
+        {
+            this.Id = id;
+        }
+
+        public Customer(int id, string name)
+            :this(id) // takes previous constructor 
+        {
+            this.Name = name; 
+        }
+
+   
+        public void Promote() 
+        {
+            
+            //...
+        }
+
+
+    }
+}
