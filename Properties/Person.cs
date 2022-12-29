45546a50df2b7745973b31c3e524760cf60471af
Slcode92 <84865384+Slcode92@users.noreply.github.com>
2022-12-29 23:18:30 +0000

    Properties

---
+++ Properties/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Properties
+{
+    public class Person 
+    {
+
+        public string Name { get; set; }
+        public string Username { get; set; }
+
+        
+
+        public DateTime Birthdate { get; private set; } //auto implemented property 
+
+      
+        
+        public Person(DateTime birthdate)
+        {
+            Birthdate = birthdate;
+        }
+
+
+      
+        
+        public int Age 
+        {
+            get 
+            {
+                TimeSpan timeSpan = DateTime.Today - Birthdate;
+                int years = timeSpan.Days / 365;
+                return years;
+            }       
+        }
+    
+    }
+}
