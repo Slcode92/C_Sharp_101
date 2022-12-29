5003bd82b1d6292aa616d240fba9dd9938669ae9
Slcode92 <84865384+Slcode92@users.noreply.github.com>
2022-12-29 22:56:50 +0000

    Understanding constructors

---
+++ Constructors/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+
+    class Program
+    {
+
+        
+        static void Main(string[] args)
+        {
+            var customer = new Customer(1,"Suraj");            
+            Console.WriteLine(customer.Id); 
+            Console.WriteLine(customer.Name);
+
+            Order order = new Order();
+            customer.Orders.Add(order);
+        }
+    }
+}
---
+++ Constructors/customer.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+using System.Collections.Generic;
+
+
+namespace Constructors
+{
+    public class Customer 
+    {
+        public int Id;
+        public string Name;
+        public List<Order> Orders;//takes objects in order class as parameter - list called "Orders"
+         
+
+
+      
+        public Customer() //default and parameterless constructor
+        { 
+            Orders = new List<Order>();
+        }
+
+
+        public Customer(int id) //inserted 1  parameter 
+            :this()//this is going to call the contructor without params 1st -- the one above 
+        {
+            this.Id = id;
+            
+        }
+
+        public Customer(int id, string name) //inserted parameter
+            :this(id)
+        {
+            this.Name = name;
+
+        }
+    }
+}
