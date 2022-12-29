a7a8a398c314da4413b8bf0fd09222b94f6aed55
Slcode92 <84865384+Slcode92@users.noreply.github.com>
2022-12-29 22:55:53 +0000

    Understanding Inheritance - OOP

---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Text text = new Text();
+            text.Width = 100; //text class usuing PresentationObject (Parent class) field
+            text.Copy();
+            text.Duplicate();
+            text.AddHyperLink("www.test123.co.uk");
+        }
+    }
+}
---
+++ Inheritance/Inheritance/Text.cs
@@ -0,0 +1,16 @@
+﻿namespace Inheritance
+{
+    public class Text : PresentationObject  // Child class  : Parent class -- Inheritance 
+    {
+        public int FontSize { get; set; }
+        public string FontName { get; set; }
+
+        public void AddHyperLink(string url)
+        {
+            System.Console.WriteLine("We added a link to " + url);
+
+        }
+
+
+    }
+}
