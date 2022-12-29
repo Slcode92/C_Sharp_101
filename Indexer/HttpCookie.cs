949259fff2f105773b4a20a4b43e75eb3a664abc
Slcode92 <84865384+Slcode92@users.noreply.github.com>
2022-12-29 22:58:22 +0000

    Working with Indexers

---
+++ Indexer/HttpCookie.cs
@@ -0,0 +1,32 @@
+﻿ using System.Collections.Generic;
+
+namespace Indexer
+{
+    public class HttpCookie
+    {
+        //specifing keys should be type string, and value should be type string 
+        //name of dictionary = "_dictionary"
+        
+        private readonly Dictionary<string, string> _dictionary;
+        
+        public HttpCookie()
+        {
+            _dictionary = new Dictionary<string, string>();
+        }
+
+
+
+
+        //now creating a indexer - does not have a name, we use this keyword 
+        //we use string to represent the keys 
+        public string this[string key]
+        {
+            get { return _dictionary[key]; }
+
+
+            set { _dictionary[key] = value; }
+        }
+
+
+    }
+}
---
+++ Indexer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexer
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpCookie cookie = new HttpCookie();
+            cookie["name"] = "Mosh"; //name is key and mosh is value 
+            Console.WriteLine(cookie["name"]);
+        }
+    }
+}
